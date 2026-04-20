de6ddb6045725f142352c9e121122b1fe957678a
duduSC <dudu.camargo104@gmail.com>
2026-04-19 22:56:21 -0300

    teste

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
 import streamlit as st
+import io  # Ferramenta do Python para lidar com arquivos na memória
+from src.remove_and_save import RemoveAndSave
+from PIL import Image
+from rembg import remove
 
-st.title("🔍 Iniciando o Aplicativo...")
+def process(imagem) -> Image:
+        try:
+            new_image = remove(imagem)
+            return new_image
+        except Exception as error:
+            print(error)
 
-try:
-    st.write("Passo 1: Tentando importar as ferramentas...")
-    from src.interface import Interface
-    
-    st.write("Passo 2: Ferramentas importadas! Carregando a interface...")
-    interface = Interface()
-    
-    st.write("Passo 3: Desenhando a tela do usuário...")
-    interface.tela()
-    
-except Exception as erro:
-    # Se algo der errado nos passos acima, a tela fica vermelha e mostra o erro!
-    st.error(f"🚨 Ops, o radar detectou um erro: {erro}")
+def read(path):
+         return Image.open(path)
+def tela():
+    # 1. Título do aplicativo
+    st.title("Removedor de Fundo de Eduardo Camargo")
+    st.write("Faça o upload de uma imagem e clique no botão para retirar o fundo!")
+
+    # 2. Cria o componente para o usuário escolher o arquivo do computador
+    arquivo_enviado = st.file_uploader(
+        "Escolha uma imagem", type=["png", "jpg", "jpeg"]
+    )
+
+    if arquivo_enviado is not None:
+
+        image = read(arquivo_enviado)
+
+        # Mostra a imagem original na tela
+        st.image(image, caption="Sua Imagem Original", use_container_width=True)
+
+        # botão de ação.
+        if st.button("Remover Fundo da Imagem"):
+
+            # Mostra um ícone de carregamento enquanto o computador pensa
+            with st.spinner("Carregando..."):
+                try:
+        
+                    imagem_sem_fundo = process(image)
+                    if imagem_sem_fundo:
+                        st.success("Fundo removido com sucesso!")
+                        st.image(
+                            imagem_sem_fundo,
+                            caption="Resultado (sem fundo)",
+                            use_container_width=True,
+                        )
+                        buffer = io.BytesIO()
+                        imagem_sem_fundo.save(buffer, format="PNG")
+                        # --- BÔNUS: Botão para baixar a nova imagem ---
+        
+                        st.download_button(
+                            label="📥 Baixar Imagem Pronta",
+                            data=buffer.getvalue(),
+                            file_name="imagem_sem_fundo.png",
+                            mime="image/png",
+                        )
+                except Exception as error:
+                    st.error(f"Erro {error}")
+
+tela()
