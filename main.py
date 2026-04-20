044acf525b7aaf789cb113c7326b729b4758ed55
duduSC <dudu.camargo104@gmail.com>
2026-04-19 22:47:10 -0300

    fix

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import streamlit as st
+st.title("Oi, eu estou vivo!") # <-- Nossa linha de teste
+
 from src.interface import Interface
 
 def main():
-
     interface = Interface()
     interface.tela()
+
 if __name__ == "__main__":
     main()
